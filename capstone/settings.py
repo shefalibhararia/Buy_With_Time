8a558abd21f7b7ee50d11411e47623c017240947
priyanshujoshilpj <joshipriyanshu85@gmail.com>
2021-11-17 06:02:45 +0530

    Added Signin and Signup functionality

--- capstone/settings.py
+++ capstone/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'capstone.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
